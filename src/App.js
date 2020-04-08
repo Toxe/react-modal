e013e8a3938d46be583908d20ecbf80b43037f7c
Toxe <toxe@toxe.net>
2020-04-08 12:32:32 +0200

    Basic page layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function App() {
-    return <div>App</div>;
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">React Modal Example</h1>
+            <p className="lead">A simple Modal using React.</p>
+            <hr className="my-4" />
+            <p>Click "Show Modal" to show a Modal and enter some values.</p>
+            <dl className="row p-4">
+            <dt className="col-sm-1">First name</dt>
+                <dd className="col-sm-11"><em>Test</em></dd>
+                <dt className="col-sm-1">Last name</dt>
+                <dd className="col-sm-11"><em>Test</em></dd>
+                <dt className="col-sm-1">Counter</dt>
+                <dd className="col-sm-11"><em>10</em></dd>
+            </dl>
+            <button type="button" className="btn btn-primary">Show Modal</button>
+        </div>
+    );
 }
