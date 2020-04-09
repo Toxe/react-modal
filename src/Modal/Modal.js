3a5e03b1676a3770b28fca30717adcee72127495
Toxe <toxe@toxe.net>
2020-04-09 15:41:33 +0200

    Remove aria attributes

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -40,22 +40,15 @@ export default function Modal(props) {
     }, []);
 
     return (
-        <div
-            ref={modalRef}
-            className="modal fade"
-            id="exampleModal"
-            tabIndex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
+        <div ref={modalRef} className="modal fade" id="exampleModal" tabIndex="-1" role="dialog">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title" id="exampleModalLabel">
                             {props.title}
                         </h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+                        <button type="button" className="close" data-dismiss="modal">
+                            <span>&times;</span>
                         </button>
                     </div>
                     <div className="modal-body">
