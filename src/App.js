ecfba628238cff9adb9364242596ad63669491ef
Toxe <toxe@toxe.net>
2020-04-09 15:22:00 +0200

    Move content into its own component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
-import React, { useState } from "react";
-import Modal from "./Modal/Modal";
+import React from "react";
+import Content from "./Content";
 
 export default function App() {
-    const [firstName, setFirstName] = useState("Test");
-    const [lastName, setLastName] = useState("Test");
-    const [counter, setCounter] = useState(10);
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const onSubmit = (values) => {
-        console.log("App.onSubmit()");
-        setFirstName(values.firstName);
-        setLastName(values.lastName);
-        setCounter(values.counter);
-        setModalVisible(false);
-    };
-
-    const onHidden = () => {
-        console.log("App.onHidden()");
-        setModalVisible(false);
-    };
-
     return (
         <div className="jumbotron">
             <h1 className="display-4">React Modal Example</h1>
             <p className="lead">A simple Modal using React.</p>
             <hr className="my-4" />
-            <p>Click "Show Modal" to show a Modal and enter some values.</p>
-            <dl className="row p-4">
-                <dt className="col-sm-1">First name</dt>
-                <dd className="col-sm-11">
-                    <em>{firstName}</em>
-                </dd>
-                <dt className="col-sm-1">Last name</dt>
-                <dd className="col-sm-11">
-                    <em>{lastName}</em>
-                </dd>
-                <dt className="col-sm-1">Counter</dt>
-                <dd className="col-sm-11">
-                    <em>{counter}</em>
-                </dd>
-            </dl>
-            <button type="button" className="btn btn-primary" onClick={() => setModalVisible(true)}>
-                Show Modal
-            </button>
-            {modalVisible && (
-                <Modal
-                    onSubmit={onSubmit}
-                    onHidden={onHidden}
-                    firstName={firstName}
-                    lastName={lastName}
-                    counter={counter}
-                />
-            )}
+            <Content />
         </div>
     );
 }
---
+++ src/Content.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Modal from "./Modal/Modal";
+
+export default function Content() {
+    const [firstName, setFirstName] = useState("Test");
+    const [lastName, setLastName] = useState("Test");
+    const [counter, setCounter] = useState(10);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const onSubmit = (values) => {
+        console.log("App.onSubmit()");
+        setFirstName(values.firstName);
+        setLastName(values.lastName);
+        setCounter(values.counter);
+        setModalVisible(false);
+    };
+
+    const onHidden = () => {
+        console.log("App.onHidden()");
+        setModalVisible(false);
+    };
+
+    return (
+        <>
+            <p>Click "Show Modal" to show a Modal and enter some values.</p>
+            <dl className="row p-4">
+                <dt className="col-sm-1">First name</dt>
+                <dd className="col-sm-11">
+                    <em>{firstName}</em>
+                </dd>
+                <dt className="col-sm-1">Last name</dt>
+                <dd className="col-sm-11">
+                    <em>{lastName}</em>
+                </dd>
+                <dt className="col-sm-1">Counter</dt>
+                <dd className="col-sm-11">
+                    <em>{counter}</em>
+                </dd>
+            </dl>
+            <button type="button" className="btn btn-primary" onClick={() => setModalVisible(true)}>
+                Show Modal
+            </button>
+            {modalVisible && (
+                <Modal
+                    onSubmit={onSubmit}
+                    onHidden={onHidden}
+                    firstName={firstName}
+                    lastName={lastName}
+                    counter={counter}
+                />
+            )}
+        </>
+    );
+}
